f0a98a7b82c1af8b33f55f4a1c4190dbf3dc163e
Areesha <areesha.fasih14@gmail.com>
2026-06-04 19:28:46 +0500

    fix: make run_training_pipeline self-contained and simplify workflow paths

--- run_training_pipeline.py
+++ run_training_pipeline.py
@@ -1,5 +1,16 @@
-# run_training_pipeline.py
-from training_pipeline import load_data, train_random_forest, train_xgboost, train_ridge
+import os
+import sys
+
+# Dynamically find the absolute path of the training_pipeline folder and add it to sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+subfolder_dir = os.path.join(current_dir, "training_pipeline")
+sys.path.append(subfolder_dir)
+
+# Now these imports will resolve flawlessly regardless of where the script is executed from
+import load_data
+import train_random_forest
+import train_xgboost
+import train_ridge
 
 def run():
     print("Loading data and starting training...")
